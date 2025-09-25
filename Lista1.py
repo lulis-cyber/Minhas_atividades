7cdf222a8f3255e8d53922a99141c64ed199643d
Luana feliciano <luanafeliciano2017.1@gmail.com>
2025-09-24 23:13:36 -0300

    primeiro commit

---
+++ Lista1.py
@@ -0,0 +1,72 @@
+
+# 1) Crie um programa que receba os valores de três notas de um aluno e imprima a média.
+
+# x = int(input("Informe a primeira nota: "))
+# y = int(input("Informe a segunda nota: "))
+# z = int(input("Informe a terceira nota: "))
+
+# media = (x+y+z)/3
+# print(f'a media é: {round(media,2)}')
+
+# 2) Converta a temperatura de Celsius para Fahrenheit.
+
+# c = float(input("Informe a temperatura em Celsius: "))
+# F = (c*1.8)+32
+# print(f"A temperatura em Fahrenheit é: {F}")
+
+# 3) Peça dois números ao usuário e mostre qual é o maior ou se são iguais.
+
+# n1 = int(input("Informe o primeiro numero: "))
+# n2 = int(input("Informe o segundo numero: "))
+
+# if n1 > n2:
+#     print("O primeiro numero é maior")
+# if n1<n2:
+#     print("O segundo numero é maior")
+# else:
+#     print("Ambos possuem o mesmo valor")
+
+# 4) Peça dois números ao usuário, calcule a soma e verifique se é maior que 20.
+
+# x = int(input("Informe o primeiro numero: "))
+# y = int(input("Informe o segundo numero: "))
+
+# z = x+y-20
+
+# if z>0:
+#     print("Maior")
+# else:
+#     print("Menor")
+
+# 5) Calcule a média de 3 notas e verifique se é igual a 7.0.
+# x = []
+# for i in range(3):
+#     x.append(int(input("Digite a nota: ")))
+
+# nota = "igual" if sum(x)==21 else "diferente" 
+# print(nota)
+# #print(sum(x))
+# 6)Peça um número inteiro e verifique se o resto da divisão por 3 é igual a 1.
+
+# 7) Peça a base a altura de um triângulo e calcule sua área.
+# base = int(input("base: "))
+# altura = int(input("altura: "))
+# area = base * altura
+
+# print(area)
+# 8) Peça um valor de hora e converta um valor em horas para minutos.
+
+
+# 9) Um funcionário foi sorteado para receber um aumento de 15% em seu salário. Calcule o valor do novo salário deste funcionário.
+
+# 10) Peça dois números inteiros e mostre o resultado da divisão inteira e o resto.
+
+# 11) Peça um número e calcule o seu quadrado e sua raiz quadrada.
+
+
+x = int(input("Informe a hora: "))
+minutos = x*60
+
+print(f"{minutos}")
+
+print(1+2)
---
+++ Lista6.py
@@ -0,0 +1,21 @@
+'''
+Questão 1) Funções Básicas
+Crie uma função chamada `dobrar` que recebe um número como parâmetro e retorna o
+dobro desse valor. Teste a função com o número 5.
+'''
+
+'''def dobrar(x): #função para calcular o dobro de um numero qualquer
+    return 2*x  #retorna o valor da função
+
+x = int(input()) #entrada do numero
+print(f" o dobro de {x} eh {dobrar(x)}") #printar resultado da função
+'''
+
+
+'''Questão 2) 2. Classe Simples
+Crie uma classe `Livro` com os atributos `titulo` e `autor`. Adicione um método
+`exibir_dados` que retorna uma string no formato: `"Título: [titulo], Autor: [autor]"`. Instancie
+um livro com título "1984" e autor "George Orwell", e chame o método.
+''' 
+
+
